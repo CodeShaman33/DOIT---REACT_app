07f92d703d3471edb9eb6a336537ff9b9cac35ab
CodeShaman33 <mar.donczyk@gmail.com>
2023-03-22 19:36:11 +0100

    add browser worker and handlers file

---
+++ src/mocks/handlers/index.js
@@ -0,0 +1,8 @@
+// import {rest} from 'msw';
+
+
+export const handlers = [
+    // rest.get('/groups', (req, res, ctx) => {
+    //     return res(ctx.status(200), ctx.json({ groups }));
+    //   }),
+]
