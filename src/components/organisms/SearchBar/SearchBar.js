1ed97ae8bcbdb9a089c4738f74dbf2163672c594
CodeShaman33 <mar.donczyk@gmail.com>
2023-03-22 17:48:21 +0100

    add search bar

---
+++ src/components/organisms/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+//style
+import  {SearchBarWrapper, SearchButton} from './SearchBar.style';
+import { Input } from "components/atoms/Input/Input";
+
+const SearchBar = () => {
+  return (
+    <SearchBarWrapper>
+      <Input />
+      <SearchButton>Find it !</SearchButton>
+    </SearchBarWrapper>
+  );
+};
+
+export default SearchBar;
--- src/components/templates/MainTemplate.js
+++ src/components/templates/MainTemplate.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Wrapper } from "./MainTemplate.style.";
 //components
 import Navigation from "components/organisms/Navigation/Navigation";
+import SearchBar from "components/organisms/SearchBar/SearchBar";
 
 
 const MainTemplate = ({children}) => {
@@ -9,6 +10,7 @@ const MainTemplate = ({children}) => {
     return (
         <Wrapper>
             <Navigation />
+            <SearchBar />
             {children}
         </Wrapper>
     )
