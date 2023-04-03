65216b9a352ba8af1780a6f02390f93a9156d683
CodeShaman33 <mar.donczyk@gmail.com>
2023-04-03 13:54:07 +0200

    add two main views to root, which dependds  on user

--- src/views/Root.js
+++ src/views/Root.js
@@ -3,23 +3,56 @@ import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "assets/styles/GlobalStyle";
 import { theme } from "assets/styles/theme";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import { Wrapper, FormWrapper } from "./Root.style";
 //components
 import MainTemplate from "components/templates/MainTemplate";
 import Tasks from "./Tasks";
+
+//authenticated app
+const AuthenticatedApp = () => {
+  return (
+    <Wrapper>
+      <MainTemplate>
+        <Switch>
+          <Route path="/" exact>
+            <Tasks />
+          </Route>
+        </Switch>
+      </MainTemplate>
+    </Wrapper>
+  );
+};
+
+//UnauthenticatedApp
+const UnauthenticatedApp = () => {
+  return (
+    <FormWrapper>
+      <form action="">
+        <label htmlFor="login" name="login" id="loginLabel">
+          Enter login:
+        </label>
+        <input type="text" id="login" />
+        <label htmlFor="password" name="password" id="passwordLabel">
+          Enter password:
+        </label>
+        <input type="password" id="password" />
+        <button type="submit" id="formButton">Log In</button>
+      </form>
+    </FormWrapper>
+  );
+};
+
 function Root() {
-  
+  const [user, setUser] = React.useState(null)
+
+  const login = (data) => {
+    
+  }
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <MainTemplate>
-          <Switch>
-            <Route path="/" exact>
-              <Tasks />
-            </Route>
-          </Switch>
-        </MainTemplate>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
       </ThemeProvider>
     </Router>
   );
