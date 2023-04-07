25081b1e0867e87ff6a21291a4b307950fe46cc5
CodeShaman33 <mar.donczyk@gmail.com>
2023-04-07 11:06:24 +0200

    add interceptor, that gets token from localstorage and adds it to axios action

--- src/hooks/useTasks.js
+++ src/hooks/useTasks.js
@@ -2,18 +2,40 @@ import axios from "axios";
 // import { response } from "msw";
 import { useEffect, useState } from "react";
 
+//this fonction, called interceptor adds token to every axios action, and in that way validate avery endpoint
+
 const tasksAPI = axios.create({});
 
-export const useTasks = () => {
-    const [tasks, setTasks] = useState([]);
-
-useEffect(() => {
-        axios.get('/')
-        .then(response => {setTasks(response.data.allTasks)} )
-        .catch(err => {console.log(err)});
-    }, []);
-
-    return {
-        tasks,
+tasksAPI.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.authorization = `Bearer ${token}`;
     }
-}
+
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+export const useTasks = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    tasksAPI
+      .get("/")
+      .then((response) => {
+        setTasks(response.data.allTasks);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return {
+    tasks,
+  };
+};
