b80fd22262c708cc29d69046e69a1f2885ed1f4d
CodeShaman33 <mar.donczyk@gmail.com>
2023-04-11 21:11:44 +0200

    migrate store to redux toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,78 +1,103 @@
 //those actions are not muting existing state, they copy all of exiting state and works on them
-import { createStore } from "redux"
+// import { createStore } from "redux"
 import {v4 as uuid } from 'uuid'
-
-export const addNote = (payload) => {
-    return {
-        type: 'notes/add',
-        payload: {
-            id: uuid(),
-            ...payload,
-        }
-    }
-}
-
-export const addTask = (payload) => {
-    return {
-        type: 'tasks/add',
-        payload: {
-            id: uuid(),
-            ...payload,
-        }
-    }
-}
-
-
-
-export const removeNote = (payload) => {
-    return {
-        type: 'notes/remove',
-        payload
-    }
-}
-
-const initialState = {
-    notes: [
+// import axios from "axios"
+import { createSlice, configureStore } from "@reduxjs/toolkit"
+const initialNotesState =  [
         {
             id: uuid(),
             title: 'first note lorem ipsum',
             content: 'first note lorem ipsum content'
         }, 
-       
-    ],
-    tasks: [
-        {
-            id: uuid(),
-            content: 'first task orem ipsum content',
-            priority: 1,
-            // done: false,
-        }
     ]
-}
 
-const notesReducer = (state = initialState, action) => {
-     switch(action.type) {
-        case 'notes/add': 
-        return {
-            ...state,
-            notes: [...state.notes, action.payload]
-        }
+const initialTasksState = [
+    {
+        id: uuid(),
+        content: 'first task orem ipsum content',
+        priority: 1,
+        // done: false,
+    }
+]
 
-        case 'notes/remove':
-            return {
-                ...state,
-                notes: state.notes.filter(note => note.id !== action.payload.id)
-            }
 
-        case 'tasks/add': 
-            return {
-                ...state,
-                tasks: [...state.tasks, action.payload]
 
-            }
-        default: 
-        return state; 
-     }
-}
 
-export const store = createStore(notesReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) 
+
+// export const addNote = (payload) => {
+//     return {
+//         type: 'notes/add',
+//         payload: {
+//             id: uuid(),
+//             ...payload,
+//         }
+//     }
+// }
+
+// export const addTask = (payload) => {
+//     return {
+//         type: 'tasks/add',
+//         payload: {
+//             id: uuid(),
+//             ...payload,
+//         }
+//     }
+// }
+
+
+
+// export const removeNote = (payload) => {
+//     return {
+//         type: 'notes/remove',
+//         payload
+//     }
+// }
+
+
+
+const notesSlice = createSlice({
+    name: 'notes',
+    initialState: initialNotesState,
+    reducers: {
+        addNote(state, action) {
+            state.push({
+                id: uuid(),
+                ...action.payload,
+            })
+        },
+        removeNote(state, action) {
+            return state.filter(note => note.id !== action.payload.id)
+        },
+    }
+
+})
+
+const tasksSlice = createSlice({
+    name: 'tasks',
+    initialState: initialTasksState,
+    reducers: {
+        addTask(state, action) {
+            state.push({
+                id: uuid(),
+                ...action.payload,
+            })
+        },
+        removeTask(state, action) {},
+    }
+
+})
+
+
+
+export const {addNote, removeNote} = notesSlice.actions
+export const {addTask, removeTask} = tasksSlice.actions
+
+
+export const store = configureStore({
+
+    reducer: {
+        notes: notesSlice.reducer,
+        tasks: tasksSlice.reducer
+
+    }
+})
